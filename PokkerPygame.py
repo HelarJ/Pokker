c37832782e096c42126337bb781b03ebe1e0200c
SilverKolde <silverkolde@gmail.com>
2019-10-24 12:17:30 +0300

    .

--- PokkerPygame.py
+++ PokkerPygame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from sys import exit
 class pokkeriPõhi:
     def __init__(self):
@@ -7,6 +8,13 @@ class pokkeriPõhi:
         ekraani_kõrgus = 600
         self.aken = pygame.display.set_mode((ekraani_laius,ekraani_kõrgus))
         self.fpsKell = pygame.time.Clock()
+        self.a = False
+        self.b = False
+        self.kaardid = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+               '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+               '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+               '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠',]
+        self.uued = self.kaardid.copy()
     
     def pokkeriKordus(self):
         while True:
@@ -14,11 +22,30 @@ class pokkeriPõhi:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-            kaart = pygame.image.load("kaardid\/ärtu2.png")
+            kaart = pygame.image.load("kaardid/ärtu2.png")
             kaart = pygame.transform.rotozoom(kaart, 0, 0.15)
             self.aken.blit(kaart, (0, 0))
             pygame.display.update()
             self.fpsKell.tick(30)
+            if not self.a:
+                self.a = self.käsi()
+                print(self.a)
+            if not self.b:
+                self.b = self.käsi()
+                print(self.b)
+            
+    def joonista_kaardid(self):
+        kaardid = self.a
+        for kaart in kaardid:
+            
+            
+    def käsi(self):
+        käsi = []
+        for i in range(5):
+            k1 = random.choice(self.uued)
+            self.uued.pop(self.uued.index(k1))
+            käsi.append(k1)
+        return käsi
 
 põhiaken = pokkeriPõhi()
 põhiaken.pokkeriKordus()
