28237898317191ac24a921221722d70b6746d5c1
SilverKolde <silverkolde@gmail.com>
2019-10-26 20:50:33 +0300

    Kaartide jaotamine mängijatele + laudu
    Praegu jagab kahele mängijale kaardid ja klikiga paneb flopi maha.
    +käe tugevus kõrge kaardi puhul parandatud

--- PokkerPygame.py
+++ PokkerPygame.py
@@ -12,6 +12,7 @@ class pokkeriPõhi:
         self.fpsKell = pygame.time.Clock()
         self.a = False
         self.b = False
+        self.c = False
         self.atugevus = 0
         self.btugevus = 0
         self.kaardid = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
@@ -28,39 +29,66 @@ class pokkeriPõhi:
         for nimi in kaardid:
             kaart = pygame.image.load("Kaardid/"+nimi+".png")
             kaart = pygame.transform.rotozoom(kaart, 0, 0.1)
-            self.aken.blit(kaart, (i+100, 100))
+            self.aken.blit(kaart, (i+325, 50))
             i += 150
         kaardid = self.b
         i = 0
         for nimi in kaardid:
             kaart = pygame.image.load("Kaardid/"+nimi+".png")
             kaart = pygame.transform.rotozoom(kaart, 0, 0.1)
-            self.aken.blit(kaart, (i+100, 300))
+            self.aken.blit(kaart, (i+325, 400))
             i += 150
+        
+        
+    def joonista_flop(self):
+        kaardid = self.c[:3]
+        i = 0
+        for nimi in kaardid:
+            kaart = pygame.image.load("Kaardid/"+nimi+".png")
+            kaart = pygame.transform.rotozoom(kaart, 0, 0.1)
+            self.aken.blit(kaart, (i+100, 225))
+            i += 150
+    
+    def joonista_turn(self):
+        kaart = pygame.image.load("Kaardid/"+self.c[3]+".png")
+        kaart = pygame.transform.rotozoom(kaart, 0, 0.1)
+        self.aken.blit(kaart, (400, 225))
+    
+    def joonista_river(self):
+        kaart = pygame.image.load("Kaardid/"+self.c[4]+".png")
+        kaart = pygame.transform.rotozoom(kaart, 0, 0.1)
+        self.aken.blit(kaart, (550, 225))
 
-    def joonista_tekst(self):
-        if self.atugevus[1]>self.btugevus[1]:
-            võitja = "A on võitja"
-        elif self.atugevus[1]<self.btugevus[1]:
-            võitja = "B on võitja"
-        else:
-            võitja = "Viik"
+#    def joonista_tekst(self):
+#        if self.atugevus[1]>self.btugevus[1]:
+#            võitja = "A on võitja"
+#        elif self.atugevus[1]<self.btugevus[1]:
+#            võitja = "B on võitja"
+#        else:
+#            võitja = "Viik"
 
-        self.aken.blit(self.font.render(võitja, False, (255, 255, 255)), (10,10))
+#        self.aken.blit(self.font.render(võitja, False, (255, 255, 255)), (10,10))
 
-        self.aken.blit(self.font.render(self.atugevus[0], False, (255, 255, 255)), (300,50))
+#        self.aken.blit(self.font.render(self.atugevus[0], False, (255, 255, 255)), (300,50))
 
-        self.aken.blit(self.font.render(self.btugevus[0], False, (255, 255, 255)), (300,250))
+#        self.aken.blit(self.font.render(self.btugevus[0], False, (255, 255, 255)), (300,250))
         
 
     def käsi(self):
         käsi = []
-        for i in range(5):
+        for i in range(2):
             k1 = random.choice(self.uued)
             self.uued.pop(self.uued.index(k1))
             käsi.append(k1)
         return käsi
 
+    def lauaKaardid(self):
+        laud = []
+        for i in range(5):
+            k1 = random.choice(self.uued)
+            self.uued.pop(self.uued.index(k1))
+            laud.append(k1)
+        return laud
 
     def pokkeriKordus(self):
         while True:
@@ -71,15 +99,28 @@ class pokkeriPõhi:
             if not self.a:
                 self.a = self.käsi()
                 print(self.a)
-                self.atugevus = pokker.käsi(self.a[0], self.a[1], self.a[2], self.a[3], self.a[4])
-                print(self.atugevus)
+#                self.atugevus = pokker.käsi(self.a[0], self.a[1], self.a[2], self.a[3], self.a[4])
+#                print(self.atugevus)
             if not self.b:
                 self.b = self.käsi()
                 print(self.b)
-                self.btugevus = pokker.käsi(self.b[0], self.b[1], self.b[2], self.b[3], self.b[4])
-                print(self.btugevus)
+#                self.btugevus = pokker.käsi(self.b[0], self.b[1], self.b[2], self.b[3], self.b[4])
+#                print(self.btugevus)
+            if not self.c:
+                self.c = self.lauaKaardid()
+                print(self.c)
             self.joonista_kaardid()
-            self.joonista_tekst()
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.joonista_flop()
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.juunista_turn()
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.juunista_river()
+                
+#            self.joonista_tekst()
             pygame.display.update()
             self.fpsKell.tick(30)
 
--- pokker.py
+++ pokker.py
@@ -89,12 +89,13 @@ def käsi(k1, k2, k3, k4, k5):
     s = sorteeri(k1, k2, k3, k4, k5)
     k1 = s[0][1]
     k2 = s[1][1]
-    k3 = s[2][1]
-    k4 = s[3][1]
-    k5 = s[4][1]
+#    k3 = s[2][1]
+#    k4 = s[3][1]
+#    k5 = s[4][1]
 
     parim = "Kõrge kaart"
-    tugevus = 0
+    tugevus = float(s[1][0])/100 + float(s[0][0])/10**3 
+    
     if yks_paar(k1, k2, k3, k4, k5):
         parim = "Üks paar"
         tugevus = 1 + (float(yks_paar(k1, k2, k3, k4, k5)))/100
@@ -122,7 +123,5 @@ def käsi(k1, k2, k3, k4, k5):
         if kmastirida(k1, k2, k3, k4, k5):
             parim = "Kuninglik mastirida"
             tugevus = 9.0
-    else:
-        tugevus = float(s[0][0])/100
         
     return (parim, tugevus)
